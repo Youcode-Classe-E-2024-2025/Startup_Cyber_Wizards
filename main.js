92f38acf7ac3df2af076a729f566cff29fa759c2
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-10-25 11:36:44 +0100

    add a js function that switch the images in the product page

--- main.js
+++
@@ -1,8 +0,0 @@
-const mainImage = document.querySelector('.mainimage');
-const thumbnails = document.querySelectorAll('.more_images img');
-
-thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener('click', function() {
-    mainImage.src = this.src;
-  });
-});
